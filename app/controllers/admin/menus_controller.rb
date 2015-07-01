9637818d1b745dd4bd2dbbce40169d403031d14c
Daniel Londoño <danielpunk@gmail.com>
2015-07-01 08:39:44 -0500

    termino correcciones

--- app/controllers/admin/menus_controller.rb
+++ app/controllers/admin/menus_controller.rb
@@ -6,16 +6,51 @@ class Admin::MenusController < ApplicationController
   def index
     @menus = Menu.all
 
-
     if !params[:date].nil?
       @menu = Menu.where(:date=>params[:date])
 
       if !@menu.blank?
-        redirect_to edit_admin_menu_path(@menu[0].id)
+
+        redirect_to edit_admin_menu_path(@menu[0].id,)
+      else
+
+        redirect_to admin_menus_path(:nada=>1, :date1=>params[:date])
       end
     end
+    fechaactual =  DateTime.now.strftime("%Y-%m-%d");
 
-    @menucreados = Menu.find_by_sql('SELECT M.id, M.date, M.estado FROM `menus` M  INNER JOIN menuwoks W ON M.id = W.menu_id WHERE  M.estado = 0 GROUP BY M.id ORDER BY DATE ')
+
+
+    limite = 5
+
+    if params[:page].nil?
+      inicio = 0
+      pagina = 1
+    else
+      pagina = params[:page].to_i
+      inicio = (pagina - 1) * limite
+    end
+
+
+
+
+    @cuentaregistros = Menu.find_by_sql('SELECT M.id, M.date, M.estado FROM `menus` M  INNER JOIN menuwoks W ON M.id = W.menu_id  GROUP BY M.id ORDER BY DATE ')
+
+
+    @totalpaginas = @cuentaregistros.count / limite.to_f
+
+
+
+    @menucreados = Menu.find_by_sql('SELECT M.id, M.date, M.estado FROM `menus` M  INNER JOIN menuwoks W ON M.id = W.menu_id  GROUP BY M.id ORDER BY DATE LIMIT '+inicio.to_s+','+limite.to_s)
+
+
+
+
+
+
+
+
+    #@menucreados = Menu.all.where('estado = 0 and date>= "'+fechaactual.to_s+'"').order('DATE ASC')
   end
 
   def new
